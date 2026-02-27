7c6592b6b391699ab5e0eeb36ed31a9475a1b4b2
Xian Dev <xiandev25@gmail.com>
2026-02-27 13:38:15 +0100

    feat(wk-1/at-1/pt-1): Create the core classes
    In the library system, the core classes are the Book class and the
    Library class.

---
+++ week-1/library_system/core.py
@@ -0,0 +1,44 @@
+class Book:
+    def BooK(self, title, author):
+        self.title = title
+        self.author = author
+        self.page = page
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+    def __len__(self):
+        return self.page
+
+    def __eq__(self, other):
+        if isinstance(other, Book):
+            return self.title == other.title and self.author == other.author
+        return False
+
+class Library:
+    def __init__(self, name):
+        self.name = name
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def remove_book(self, book):
+        self.books.remove(book)
+
+    def __str__(self):
+        return f"{self.name} has {len(self.books)} books"
+
+    def __getitem__(self, index):
+        try:
+            return self.books[index]
+        except IndexError:
+            return f"Book at {index} not found"
+
+    def __len__(self):
+        return len(self.books)
+
+    def __eq__(self, other):
+        if isinstance(other, Library):
+            return self.name == other.name and self.books == other.books
+        return False
