91bd00d7bcca1e281d47760d9515a5cb4cfe3e45
Xian Dev <xiandev25@gmail.com>
2026-03-01 14:02:15 +0100

    feat(wk-1/at-1/pt-2): Use decorators and closures

--- week-1/library_system/core.py
+++ week-1/library_system/core.py
@@ -1,3 +1,6 @@
+from utils import permission_check
+from utils import track_access
+
 class Book:
     def __init__(self, title, author, page):
         self.title = title
@@ -15,23 +18,30 @@ class Book:
             return self.title == other.title and self.author == other.author
         return False
 
+    def __repr__(self):
+        return f"Book({self.title}, {self.author}, {self.page})"
+
 class Library:
     def __init__(self, name, books=None):
         self.name = name
         self.books = books if books is not None else []
 
+    @permission_check("dasd")
     def add_book(self, book):
         self.books.append(book)
 
+    @permission_check("admin")
     def remove_book(self, book):
         self.books.remove(book)
 
+    @track_access
     def borrow_book(self, book):
         if book in self.books:
             self.books.remove(book)
         else:
             raise ValueError(f"Book {book} not found in library {self.name}")
 
+    @track_access
     def return_book(self, book):
         self.books.append(book)
 
