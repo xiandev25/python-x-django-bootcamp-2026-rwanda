d1e6bf4827b414d1fda49c296f0908452c7b61e1
Xian Dev <xiandev25@gmail.com>
2026-03-04 05:26:45 +0100

    feat(wk-2/at-1/pt-1): Check recently published Questions

--- week-2/djangotutorial/polls/models.py
+++ week-2/djangotutorial/polls/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -6,6 +7,9 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
