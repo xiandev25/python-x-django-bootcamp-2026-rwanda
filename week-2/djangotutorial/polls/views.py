563206193c4252067fe08f1864decc6af63e9bbe
Xian Dev <xiandev25@gmail.com>
2026-03-06 09:18:13 +0100

    feat(wk-3/ch-2/pt-3): Shortcut template rendering
    Using the get_object_or_404() method that takes the model type as its
    first argument and any number of keyword arguments that describe the
    object to be retrieved. If it does find the object, it raises a Http404
    exception.

--- week-2/djangotutorial/polls/views.py
+++ week-2/djangotutorial/polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from .models import Question
-from django.shortcuts import render, Http404
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
     latest_question_list = Question.objects.order_by("pub_date")[:5]
@@ -10,10 +10,11 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    # try:
+    #    question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #    raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
