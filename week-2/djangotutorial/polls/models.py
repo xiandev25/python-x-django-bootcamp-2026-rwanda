2f17092e1e42f8572679c25e5aeacfa6dd46e38e
Xian Dev <xiandev25@gmail.com>
2026-03-15 15:59:31 +0100

    fix(wk-3/rd-d/pt-5): Fix missing datetime import

--- week-2/djangotutorial/polls/models.py
+++ week-2/djangotutorial/polls/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+import datetime
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -9,7 +11,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - timedelta(days=1)
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
